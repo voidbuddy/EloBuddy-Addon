d7b7c68b404176242bd1878966c56373c7156848
AlfonsCasanova <mnadzieja21@gmail.com>
2015-09-22 14:24:54 +0200

    helloworld

---
+++ CassiOPeia/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using EloBuddy.SDK.Rendering;
+using CassiOPeia.Activator;
+using Item = CassiOPeia.Activator.Item;
+
+namespace CassiOPeia
+{
+    internal class Program
+    {
+        public static Dictionary<AIHeroClient, Slider> _SkinVals = new Dictionary<AIHeroClient, Slider>();
+        public static Menu ComboMenu, HarassMenu, UltimateMenu, DrawingsMenu, menu;
+        public static Spell.Skillshot Q;
+        public static Spell.Skillshot W;
+        public static Spell.Targeted E;
+        public static Spell.Skillshot R;
+        public static AIHeroClient _Player { get { return ObjectManager.Player; } }
+        public static int Mana { get { return (int) _Player.Mana; } }
+        private static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            Hacks.AntiAFK = true;
+            Bootstrap.Init(null);
+            ItemManager.Init();
+            TargetSelector2.init();
+            Q = new Spell.Skillshot(SpellSlot.Q, 850, SkillShotType.Circular, 750, int.MaxValue, 150);
+            W = new Spell.Skillshot(SpellSlot.W, 850, SkillShotType.Circular, 250, 2500, 250);
+            E = new Spell.Targeted(SpellSlot.E, 700);
+            R = new Spell.Skillshot(SpellSlot.R, 825, SkillShotType.Cone, (int)0.6f, int.MaxValue, (int)(80 * Math.PI / 180));
+
+            menu = MainMenu.AddMenu("CassiOPeia", "CassiOPeia");
+            menu.AddGroupLabel("CassiOPeia");
+            menu.AddLabel("Made by vOID");
+            menu.AddSeparator();
+            menu.AddLabel("Changelog:");
+            menu.AddLabel("v0.01 - Initial release");
+            menu.AddLabel("v0.02 - Auto-Ultimate, Harass");
+            menu.AddLabel("v0.03 - Skin Hack");
+            menu.AddLabel("v0.04 - Deleted Skin Hack (Was causing bugsplats), Added Interrupter, Toggle Q Harass, Farm with E");
+
+            ComboMenu = menu.AddSubMenu("Combo Menu", "comboMenu");
+            
+            ComboMenu.AddGroupLabel("Combo Menu");
+            ComboMenu.Add("qCombo", new CheckBox("Use Q"));
+            ComboMenu.Add("wCombo", new CheckBox("Use W"));
+            ComboMenu.Add("eCombo", new CheckBox("Use E"));
+
+            HarassMenu = menu.AddSubMenu("Harass Menu", "harassMenu");
+            HarassMenu.Add("qHarass", new CheckBox("Use Q"));
+            HarassMenu.Add("eHarass", new CheckBox("Use E"));
+            HarassMenu.Add("qHarassToggle", new KeyBind("Q Toggle Harass", false, KeyBind.BindTypes.PressToggle, 'T'));
+
+            UltimateMenu = menu.AddSubMenu("Ultimate", "ultimateMenu");
+            UltimateMenu.Add("useautoultimate", new CheckBox("Use Auto-Ultimate"));
+            UltimateMenu.Add("ultimateinterrupt", new CheckBox("Use Ultimate to Interrupt Spells"));
+            UltimateMenu.AddSeparator();
+            UltimateMenu.Add("minR", new Slider("Minimum Enemies to Cast Ultimate", 2, 1, 5));
+
+            DrawingsMenu = menu.AddSubMenu("Drawings", "drawingsMenu");
+            DrawingsMenu.Add("drawq", new CheckBox("Draw Q Range"));
+
+            Interrupter.OnInterruptableSpell += Interrupter_OnInterruptableSpell;
+
+        }
+
+        static void Interrupter_OnInterruptableSpell(Obj_AI_Base sender, InterruptableSpellEventArgs e)
+        {
+            if (UltimateMenu["ultimateinterruptt"].Cast<CheckBox>().CurrentValue)
+            if (sender.IsValidTarget(Program.R.Range))
+                if (Program.R.IsReady())
+                    Program.R.Cast(sender);
+        }
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            if (DrawingsMenu["drawq"].Cast<CheckBox>().CurrentValue)
+                new Circle { Color = E.IsReady() ? Color.Green : Color.Red, Radius = Program.Q.Range }.Draw(ObjectManager.Player.Position);
+        }
+
+        private static void Game_OnTick(EventArgs args)
+        {
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo))
+            {
+                StateManager.Combo();
+            }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass))
+            {
+                StateManager.Harass();
+            }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LaneClear))
+            {
+                StateManager.LastHit();
+            }
+            {
+                StateManager.Ultimate();
+            }
+            {
+                StateManager.ToggleHarass();
+            }
+        }
+    }
+
+
+}
---
+++ CassiOPeia/StateManager.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu.Values;
+
+namespace CassiOPeia
+{
+    class StateManager
+    {
+        public static AIHeroClient _Player { get { return ObjectManager.Player; } }
+        private static long LastQCast = 0;
+        private static long LastECast = 0;
+
+        public static float GetDamage(SpellSlot spell, Obj_AI_Base target)
+        {
+            float ap = ObjectManager.Player.FlatMagicDamageMod + ObjectManager.Player.BaseAbilityDamage;
+            if (spell == SpellSlot.E)
+            {
+                if (!Program.E.IsReady())
+                    return 0;
+                return ObjectManager.Player.CalculateDamageOnUnit(target, DamageType.Magical, 55f + 25f * (Program.E.Level - 1) + 55 / 100 * ap);
+            }
+            return 0;
+        }
+
+        public static void Combo()
+        {
+            var target = TargetSelector2.GetTarget(850, DamageType.Magical);
+            {
+                if (Program.E.IsReady() && Program.ComboMenu["eCombo"].Cast<CheckBox>().CurrentValue && target.IsValidTarget(Program.E.Range) && target != null && target.IsVisible && !target.IsDead)
+                {
+                    if ((target.HasBuffOfType(BuffType.Poison)))
+                    {
+                        if (target.IsValidTarget(Program.E.Range))
+                        {
+                            Program.E.Cast(target);
+                        }
+                    }
+                }
+                if (Program.Q.IsReady() && Program.ComboMenu["qCombo"].Cast<CheckBox>().CurrentValue && target.IsValidTarget(Program.Q.Range))
+                {
+                    Program.Q.Cast(target);
+                }
+
+                if (Program.W.IsReady() && target.IsValidTarget(Program.W.Range) && Program.ComboMenu["wCombo"].Cast<CheckBox>().CurrentValue && Environment.TickCount > LastQCast + Program.Q.CastDelay * 1000)
+                {
+                    Program.W.Cast(target);
+                }
+            }
+        }
+        public static void Harass()
+        {
+          var target = TargetSelector2.GetTarget(850, DamageType.Magical);
+          {
+            if (Program.E.IsReady() && Program.HarassMenu["eHarass"].Cast<CheckBox>().CurrentValue && target.IsValidTarget(Program.E.Range) && target != null && target.IsVisible && !target.IsDead)
+            {
+                if ((target.HasBuffOfType(BuffType.Poison)))
+                {
+                    if (target.IsValidTarget(Program.E.Range))
+                    {
+                        Program.E.Cast(target);
+                    }
+                }
+            }
+            if (Program.Q.IsReady() && Program.HarassMenu["qHarass"].Cast<CheckBox>().CurrentValue && target.IsValidTarget(Program.Q.Range))
+            {
+                Program.Q.Cast(target);
+            }
+          }
+        }
+
+        public static void LastHit()
+        {
+            foreach (var minion in ObjectManager.Get<Obj_AI_Minion>().Where(x => Program.E.IsInRange(x)
+                && !x.IsDead
+                && x.IsEnemy
+                && x.HasBuffOfType(BuffType.Poison)
+                && x.Health + 5 < GetDamage(SpellSlot.E, x)))
+            {
+                Program.E.Cast(minion);
+            }
+        }
+
+        public static void ToggleHarass()
+        {
+            var target = TargetSelector2.GetTarget(850, DamageType.Magical);
+            {
+                if (Program.Q.IsReady() && Program.HarassMenu["qHarassToggle"].Cast<CheckBox>().CurrentValue && target.IsValidTarget(Program.Q.Range))
+                {
+                    Program.Q.Cast(target);
+                }
+            }
+        }
+
+        public static void Ultimate()
+        {
+            var target = TargetSelector2.GetTarget(500, DamageType.Magical);
+            var castPred = Program.R.GetPrediction(target); 
+            {
+                {
+                    foreach (
+                        var enemy in
+                            ObjectManager.Get<AIHeroClient>()
+                                .Where(enemy => enemy.Distance(_Player) <= Program.R.Range))
+                    {
+                        if (enemy.CountEnemiesInRange(500) >= Program.UltimateMenu["minR"].Cast<Slider>().CurrentValue && Program.UltimateMenu["useautoultimate"].Cast<CheckBox>().CurrentValue && enemy.IsFacing(ObjectManager.Player))
+                        {
+                            Program.R.Cast(enemy.Position);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
