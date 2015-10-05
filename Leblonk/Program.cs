b81f9a79b18ff2efaa04a5d1e5138238344cf2a2
AlfonsCasanova <mnadzieja21@gmail.com>
2015-10-05 17:54:40 +0200

    helloworld

---
+++ Leblonk/Program.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using EloBuddy.SDK.Rendering;
+using SharpDX;
+using Leblonk.Activator;
+using Item = Leblonk.Activator.Item;
+namespace Leblonk
+{
+    public static class Program
+    {
+        public const string ChampName = "Leblanc";
+        public static Spell.Targeted Q;
+        public static Spell.Skillshot W;
+        public static Spell.Skillshot E;
+        public static Spell.Targeted R;
+        public static Menu ComboMenu, KillstealMenu, HarassMenu, FleeMenu, DrawMenu, menu;
+        public static AIHeroClient myHero { get { return ObjectManager.Player; } }
+        public static void Main(string[] args)
+
+        {
+            Loading.OnLoadingComplete += OnLoadingComplete;
+        }
+
+        private static void OnLoadingComplete(EventArgs args)
+        {
+            if (Player.Instance.ChampionName != ChampName)
+            {
+                return;
+            }
+
+            Hacks.AntiAFK = true;
+            Bootstrap.Init(null);
+            ItemManager.Init();
+            TargetSelector2.init();
+
+            Q = new Spell.Targeted(SpellSlot.Q, 720);
+            W = new Spell.Skillshot(SpellSlot.W, 600, SkillShotType.Circular, int.MaxValue, 1450, 220);
+            E = new Spell.Skillshot(SpellSlot.E, 950, SkillShotType.Linear, 250, 1600, 70);
+            R = new Spell.Targeted(SpellSlot.R, 720);
+
+            menu = MainMenu.AddMenu("LeBlanc", "LeBlanc");
+            menu.AddGroupLabel("CassiOPeia");
+            menu.AddLabel("Made by vOID");
+
+            ComboMenu = menu.AddSubMenu("Combo Menu", "comboMenu");
+
+            ComboMenu.AddGroupLabel("Combo Menu");
+            ComboMenu.Add("qCombo", new CheckBox("Use Q"));
+            ComboMenu.AddSeparator();
+            ComboMenu.Add("wCombo", new CheckBox("Use W"));
+            ComboMenu.AddSeparator();
+            ComboMenu.Add("eCombo", new CheckBox("Use E"));
+            ComboMenu.AddSeparator();
+            ComboMenu.Add("chainz", new KeyBind("2Chainz", false, KeyBind.BindTypes.HoldActive, 'A' ));
+
+            HarassMenu = menu.AddSubMenu("Harass Menu", "HarassMenu");
+            HarassMenu.AddGroupLabel("Harass Menu");
+            HarassMenu.Add("useQ", new CheckBox("Use Q"));
+            HarassMenu.AddSeparator();
+            HarassMenu.Add("useW", new CheckBox("Use W"));
+            HarassMenu.AddSeparator();
+            HarassMenu.Add("useE", new CheckBox("Use E"));
+
+            KillstealMenu = menu.AddSubMenu("Killsteal Menu", "killstealMenu");
+            KillstealMenu.AddGroupLabel("Killsteal Menu");
+            KillstealMenu.Add("ksQ", new CheckBox("Killsteal with Q"));
+            KillstealMenu.Add("ksQR", new CheckBox("Killsteal with QR"));
+
+            FleeMenu = menu.AddSubMenu("Flee Menu", "fleeMenu");
+            FleeMenu.AddGroupLabel("Flee Menu");
+            FleeMenu.Add("useW", new CheckBox("Flee with W"));
+            FleeMenu.Add("useWR", new CheckBox("Flee with RW"));
+
+            DrawMenu = menu.AddSubMenu("Draw Menu", "drawMenu");
+            DrawMenu.Add("drawQ", new CheckBox("Draw Q"));
+            DrawMenu.Add("drawE", new CheckBox("Draw E"));
+            DrawMenu.Add("drawWQ", new CheckBox("Draw W+Q"));
+            DrawMenu.Add("killable", new CheckBox("Draw Killable"));
+
+            Game.OnTick += Game_OnTick;
+            Drawing.OnDraw += OnDraw;
+            Obj_AI_Base.OnProcessSpellCast += Obj_AI_Base_OnProcessSpellCast;
+
+        }
+
+        public static Vector3 mousePos
+        {
+            get { return Game.CursorPos; }
+        }
+        
+        private static void Game_OnTick(EventArgs args)
+        {         
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo))
+            {
+                SpellManager.Combo();
+            }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass))
+            {
+                SpellManager.Harass();
+            }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Flee))
+            {
+                SpellManager.Flee();
+            }
+            if (ComboMenu["chainz"].Cast<KeyBind>().CurrentValue)
+            {
+                SpellManager.Chainz();
+                Orbwalker.OrbwalkTo(mousePos);
+            }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.None))
+            {
+                SpellManager.Killsteal();
+            }
+
+        }
+
+        public static Dictionary<string, string> Spells = new Dictionary<string, string>
+        {
+            {"ER", "LeblancSoulShackleM"},
+            {"E", "LeblancSoulShackle"},
+            {"W", "LeblancSlide"},
+        };
+
+        public static Dictionary<string, long> LastCast = new Dictionary<string, long>
+        {
+            {"LeblancSoulShackleM", 0},
+            {"LeblancSoulShackle", 0},
+            {"LeblancSlide", 0},
+        };
+
+        private static void Obj_AI_Base_OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
+        {
+            if (!sender.IsMe)
+                return;
+
+            LastCast[Program.Spells["ER"]] = Environment.TickCount;
+            LastCast[Program.Spells["E"]] = Environment.TickCount;
+            LastCast[Program.Spells["W"]] = Environment.TickCount;
+        }
+
+        private static void OnDraw(EventArgs args)
+        {
+            var target = TargetSelector2.GetTarget(1320, DamageType.Magical);
+
+            Boolean drawQ = DrawMenu["drawQ"].Cast<CheckBox>().CurrentValue;
+            Boolean drawE = DrawMenu["drawE"].Cast<CheckBox>().CurrentValue;
+            Boolean drawWQ = DrawMenu["drawWQ"].Cast<CheckBox>().CurrentValue;
+            Boolean drawKillable = DrawMenu["killable"].Cast<CheckBox>().CurrentValue;
+
+
+            if (Q.IsReady() && drawQ)
+                Circle.Draw(SharpDX.Color.Aqua, Q.Range, Player.Instance.Position);
+            if (E.IsReady() && drawE)
+                Circle.Draw(SharpDX.Color.AliceBlue, E.Range, Player.Instance.Position);
+            if (drawWQ)
+                Circle.Draw(SharpDX.Color.BlanchedAlmond, Q.Range + W.Range, Player.Instance.Position);
+
+            foreach (AIHeroClient enemy in HeroManager.Enemies)
+            {
+                if (drawKillable && enemy.Health < SpellManager.QDamage(enemy)
+                    && Q.IsReady())
+                    Drawing.DrawText(Drawing.WorldToScreen(enemy.Position), System.Drawing.Color.White, "Q = DEAD", 200);
+
+                    if (drawKillable && enemy.Health < SpellManager.QDamage(enemy) + SpellManager.Q2Damage(enemy) + SpellManager.WDamage(enemy)
+                    && enemy.Health > SpellManager.QDamage(enemy)
+                    && Q.IsReady() && W.IsReady())
+                    Drawing.DrawText(Drawing.WorldToScreen(enemy.Position), System.Drawing.Color.White, "Q + W = DEAD", 500);
+
+                    if (drawKillable && enemy.Health < SpellManager.QDamage(enemy) + SpellManager.Q2Damage(enemy) + SpellManager.RQDamage(enemy)
+                    && enemy.Health > SpellManager.QDamage(enemy) + SpellManager.WDamage(target)
+                    && Q.IsReady() && R.IsReady())
+                    Drawing.DrawText(Drawing.WorldToScreen(enemy.Position), System.Drawing.Color.White, "Q + R = DEAD", 500);
+
+                if (drawKillable && enemy.Health < SpellManager.QDamage(enemy) + SpellManager.Q2Damage(enemy) + SpellManager.RQDamage(enemy) + SpellManager.RQ2Damage(enemy) + SpellManager.WDamage(enemy)
+                    && enemy.Health > SpellManager.QDamage(enemy) + SpellManager.Q2Damage(enemy) + SpellManager.RQDamage(enemy)
+                    && Q.IsReady() && R.IsReady() && W.IsReady())
+                    Drawing.DrawText(Drawing.WorldToScreen(enemy.Position), System.Drawing.Color.White, "Q + R + W = DEAD", 500);
+
+                if (drawKillable && enemy.Health < SpellManager.QDamage(enemy) + SpellManager.Q2Damage(enemy) + SpellManager.RQDamage(enemy) + SpellManager.RQ2Damage(enemy) + SpellManager.WDamage(enemy) + SpellManager.EDamage(enemy)
+                    && enemy.Health > SpellManager.QDamage(enemy) + SpellManager.Q2Damage(enemy) + SpellManager.RQDamage(enemy) + SpellManager.RQ2Damage(enemy) + SpellManager.WDamage(enemy)
+                    && Q.IsReady() && R.IsReady() && W.IsReady() && E.IsReady())
+                    Drawing.DrawText(Drawing.WorldToScreen(enemy.Position), System.Drawing.Color.White, "Q + R + W + E = DEAD", 500);
+
+                if (drawKillable && enemy.Health < SpellManager.QDamage(enemy) + SpellManager.Q2Damage(enemy) + SpellManager.RQDamage(enemy) + SpellManager.RQ2Damage(enemy) + SpellManager.WDamage(enemy) + SpellManager.EDamage(enemy) + SpellManager.E2Damage(enemy)
+                    && enemy.Health > SpellManager.QDamage(enemy) + SpellManager.Q2Damage(enemy) + SpellManager.RQDamage(enemy) + SpellManager.RQ2Damage(enemy) + SpellManager.WDamage(enemy) + SpellManager.EDamage(enemy)
+                    && Q.IsReady() && R.IsReady() && W.IsReady() && E.IsReady())
+                    Drawing.DrawText(Drawing.WorldToScreen(enemy.Position), System.Drawing.Color.White, "Q + R + W + E + E2 = DEAD", 500);
+            }
+        }
+    }
+}
---
+++ Leblonk/SpellManager.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu.Values;
+
+namespace Leblonk
+
+{
+    class SpellManager
+    {
+        public static float QDamage(Obj_AI_Base target)
+        {
+            return Program.myHero.CalculateDamageOnUnit(target, DamageType.Magical,
+                new[] { 55, 80, 105, 130, 155 }[Program.Q.Level - 1] +
+                0.4f * Program.myHero.FlatMagicDamageMod);
+        }
+
+        public static float Q2Damage(Obj_AI_Base target)
+        {
+            return Program.myHero.CalculateDamageOnUnit(target, DamageType.Magical,
+                new[] { 55, 80, 105, 130, 155 }[Program.Q.Level - 1] +
+                0.4f * Program.myHero.FlatMagicDamageMod);
+        }
+
+        public static float WDamage(Obj_AI_Base target)
+        {
+            return Program.myHero.CalculateDamageOnUnit(target, DamageType.Magical,
+                new[] { 85, 125, 165, 205, 245 }[Program.W.Level - 1] +
+                0.6f * Program.myHero.FlatMagicDamageMod);
+        }
+
+        public static float EDamage(Obj_AI_Base target)
+        {
+            return Program.myHero.CalculateDamageOnUnit(target, DamageType.Magical,
+                new[] { 40, 65, 90, 115, 140 }[Program.W.Level - 1] +
+                0.5f * Program.myHero.FlatMagicDamageMod);
+        }
+
+        public static float E2Damage(Obj_AI_Base target)
+        {
+            return Program.myHero.CalculateDamageOnUnit(target, DamageType.Magical,
+                new[] { 40, 65, 90, 115, 140 }[Program.W.Level - 1] +
+                0.5f * Program.myHero.FlatMagicDamageMod);
+        }
+
+        public static float RQDamage(Obj_AI_Base target)
+        {
+            return Program.myHero.CalculateDamageOnUnit(target, DamageType.Magical,
+                new[] { 100, 200, 300 }[Program.R.Level - 1] +
+                0.65f * Program.myHero.FlatMagicDamageMod);
+        }
+
+        public static float RQ2Damage(Obj_AI_Base target)
+        {
+            return Program.myHero.CalculateDamageOnUnit(target, DamageType.Magical,
+                new[] { 100, 200, 300 }[Program.R.Level - 1] +
+                0.65f * Program.myHero.FlatMagicDamageMod);
+        }
+
+        public static void Killsteal()
+        {
+            foreach (AIHeroClient enemy in HeroManager.Enemies)
+            {
+                if (enemy.IsValidTarget(Program.Q.Range))
+                {
+                    if (QDamage(enemy) >= enemy.Health && Program.KillstealMenu["ksQ"].Cast<CheckBox>().CurrentValue) { Program.Q.Cast(enemy); }
+                    if (RQDamage(enemy) >= enemy.Health && !Program.Q.IsReady()
+                        && ObjectManager.Player.Spellbook.GetSpell(SpellSlot.R).Name == "LeblancChaosOrbM"
+                        && Program.KillstealMenu["ksQR"].Cast<CheckBox>().CurrentValue)
+                    { Program.R.Cast(enemy); }
+                }
+            }
+        }
+
+        public static void Combo()
+        {
+            var target = TargetSelector2.GetTarget(1320, DamageType.Magical);
+            var jumpPoint = Program.myHero.Position.Extend(target, 600).To3D();
+            {
+                if (Program.W.IsReady() && !target.IsValidTarget(Program.W.Range)
+                && target.IsValidTarget(Program.W.Range + Program.Q.Range)
+                && ObjectManager.Player.Spellbook.GetSpell(SpellSlot.W).Name != "leblancslidereturn"
+                && Program.Q.IsReady() && Program.R.IsReady()
+                && target.Health < QDamage(target) + Q2Damage(target) + RQDamage(target))
+                {
+                    Program.W.Cast(jumpPoint);
+                }
+                if (Program.W.IsReady() && !target.IsValidTarget(Program.W.Range)
+                && target.IsValidTarget(Program.W.Range + Program.Q.Range)
+                && ObjectManager.Player.Spellbook.GetSpell(SpellSlot.W).Name != "leblancslidereturn"
+                && Program.Q.IsReady()
+                && target.Health < QDamage(target))
+                {
+                    Program.W.Cast(jumpPoint);
+                }
+
+
+                if (Program.Q.IsReady() && target.IsValidTarget(Program.Q.Range))
+                {
+                    Program.Q.Cast(target);
+                }
+
+
+                if (Program.R.IsReady() && target.IsValidTarget(Program.Q.Range) && ObjectManager.Player.Spellbook.GetSpell(SpellSlot.R).Name == "LeblancChaosOrbM")
+                {
+                    Program.R.Cast(target);
+                }
+
+
+                if (Program.W.IsReady() && target.IsValidTarget(Program.W.Range) && ObjectManager.Player.Spellbook.GetSpell(SpellSlot.W).Name != "leblancslidereturn"
+                && !Program.R.IsReady())
+
+                    Program.W.Cast(target);
+
+                else if (Program.R.IsReady() && Program.LastCast[Program.Spells["W"]] + 150 < Environment.TickCount && ObjectManager.Player.Spellbook.GetSpell(SpellSlot.W).Name != "leblancslidereturn")
+
+                    Program.W.Cast(target);
+
+
+                if (Program.E.IsReady() && target.IsValidTarget(Program.E.Range) && Program.LastCast[Program.Spells["E"]] + 150 < Environment.TickCount)
+                {
+                    var e = Program.E.GetPrediction(target);
+                    if (e.HitChance >= EloBuddy.SDK.Enumerations.HitChance.High)
+                    {
+                        var predE = Prediction.Position.PredictLinearMissile(target, Program.E.Range, 55, 250, 1600, 0);
+                        Program.E.Cast(predE.CastPosition);
+                    }
+                }
+            }
+        }
+
+        public static void ComboWR()
+        {
+
+        }
+
+        public static void Harass()
+        {
+            var target = TargetSelector2.GetTarget(720, DamageType.Magical);
+            {
+                if (Program.Q.IsReady() && target.IsValidTarget(Program.Q.Range)
+                    && Program.HarassMenu["useQ"].Cast<CheckBox>().CurrentValue)
+                {
+                    Program.Q.Cast(target);
+
+                }
+
+                if (Program.W.IsReady() && target.IsValidTarget(Program.W.Range) && ObjectManager.Player.Spellbook.GetSpell(SpellSlot.W).Name != "leblancslidereturn"
+                    && Program.HarassMenu["useW"].Cast<CheckBox>().CurrentValue)
+                {
+                    {
+                        var w = Program.W.GetPrediction(target);
+                        if (w.HitChance >= EloBuddy.SDK.Enumerations.HitChance.Low)
+                        {
+                            var predW = Prediction.Position.PredictCircularMissile(target, Program.E.Range, 500, 250, 1450);
+                            Program.W.Cast(target);
+                        }
+                    }
+                }
+
+                if (Program.E.IsReady() && target.IsValidTarget(Program.E.Range) && !Program.R.IsReady()
+                     && Program.HarassMenu["useE"].Cast<CheckBox>().CurrentValue)
+                {
+                    var e = Program.E.GetPrediction(target);
+                    if (e.HitChance >= EloBuddy.SDK.Enumerations.HitChance.High)
+                    {
+                        var predE = Prediction.Position.PredictLinearMissile(target, Program.E.Range, 55, 250, 1600, 0);
+                        Program.E.Cast(predE.CastPosition);
+                    }
+                }
+            }
+        }
+
+
+        public static void Flee()
+        {
+            var jumpPoint = Program.myHero.Position.Extend(Program.mousePos, 600).To3D();
+            if (Program.W.IsReady()
+                && Program.FleeMenu["useW"].Cast<CheckBox>().CurrentValue
+                && ObjectManager.Player.Spellbook.GetSpell(SpellSlot.W).Name != "leblancslidereturn")
+                {
+                            Program.W.Cast(jumpPoint);
+                }
+
+            if (Program.R.IsReady()
+                && Program.FleeMenu["useWR"].Cast<CheckBox>().CurrentValue 
+                && ObjectManager.Player.Spellbook.GetSpell(SpellSlot.R).Name == "LeblancSlideM"
+                && ObjectManager.Player.Spellbook.GetSpell(SpellSlot.R).Name != "leblancslidereturnm")
+            {
+                Program.R.Cast(jumpPoint);
+            }
+
+        }
+
+        public static void Chainz()
+        {       
+            var target = TargetSelector2.GetTarget(950, DamageType.Magical);
+            if (Program.E.IsReady() && target.IsValidTarget(Program.E.Range))
+                {
+                    var e = Program.E.GetPrediction(target);
+                    if (e.HitChance >= EloBuddy.SDK.Enumerations.HitChance.High)
+                    {
+                        var predE = Prediction.Position.PredictLinearMissile(target, Program.E.Range, 55, 250, 1600, 0);
+                        Program.E.Cast(predE.CastPosition);
+                    }
+                }
+            if (Program.R.IsReady() && target.IsValidTarget(Program.E.Range) && ObjectManager.Player.Spellbook.GetSpell(SpellSlot.R).Name == "LeblancSoulShackleM")
+            {
+                var e = Program.E.GetPrediction(target);
+                if (e.HitChance >= EloBuddy.SDK.Enumerations.HitChance.High && Program.LastCast[Program.Spells["ER"]] + 1000 < Environment.TickCount)
+                {
+                    var predR = Prediction.Position.PredictLinearMissile(target, Program.E.Range, 55, 250, 1600, 0);                  
+                    Program.R.Cast(predR.CastPosition);
+                    
+                }
+            }
+        }
+    }
+}
