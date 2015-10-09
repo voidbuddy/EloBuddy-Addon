97633f877f039c70e34cfc774853e1d267ed266e
AlfonsCasanova <mnadzieja21@gmail.com>
2015-10-09 13:04:57 +0200

    edited by Darakath

--- DeveloperBuddy/Program.cs
+++ DeveloperBuddy/Program.cs
@@ -1,118 +1,137 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Drawing;
-using System.Threading.Tasks;
-using EloBuddy;
-using EloBuddy.SDK;
-using EloBuddy.SDK.Enumerations;
-using EloBuddy.SDK.Events;
-using EloBuddy.SDK.Menu;
-using EloBuddy.SDK.Menu.Values;
-using EloBuddy.SDK.Rendering;
-using SharpDX;
-
-namespace DeveloperBuddy
+﻿namespace DevBuddy2._0
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Linq;
+
+    using EloBuddy;
+    using EloBuddy.SDK;
+    using EloBuddy.SDK.Events;
+    using EloBuddy.SDK.Menu;
+    using EloBuddy.SDK.Menu.Values;
+
     public static class Program
     {
-        private static List<GameObject> _lstGameObjects = new List<GameObject>();
-        private static List<GameObject> _lstObjCloseToMouse = new List<GameObject>();
-        private static Menu Config, menu;
-        private static int _lastUpdateTick = 0;
-        static void Main(string[] args)
+        private static List<GameObject> gameObjects = new List<GameObject>();
+
+        private static List<GameObject> mouseObjects = new List<GameObject>();
+
+        private static Menu cMenu, Menu;
+
+        private static int lastTick;
+
+        private static void Main(string[] args)
         {
             Loading.OnLoadingComplete += OnLoadingComplete;
         }
 
+        private static Color GetMenuColor()
+        {
+            return Color.FromArgb(
+                cMenu["colorR"].Cast<Slider>().CurrentValue,
+                cMenu["colorG"].Cast<Slider>().CurrentValue,
+                cMenu["colorB"].Cast<Slider>().CurrentValue);
+        }
+
         private static void OnLoadingComplete(EventArgs args)
         {
-
             Hacks.AntiAFK = true;
             Bootstrap.Init(null);
 
-            menu = MainMenu.AddMenu("devbuddy", "DeveloperBuddy");
-            menu.AddGroupLabel("ヽ༼ຈل͜ຈ༽ﾉ RAISE YOUR DONGERS ヽ༼ຈل͜ຈ༽ﾉ ");
-            menu.AddLabel("PORTED FROM YOU KNOW WHERE");
-            menu.AddLabel("Ported by vOID github.com/voidbuddy");
-            menu.AddLabel("www.elobuddy.net/user/188-void/");
+            Menu = MainMenu.AddMenu("DevBuddy2", "DevBuddy2.0");
+            Menu.AddGroupLabel("ヽ༼ຈل͜ຈ༽ﾉ RAISE YOUR DONGERS ヽ༼ຈل͜ຈ༽ﾉ ");
+            Menu.AddLabel("PORTED FROM YOU KNOW WHERE");
+            Menu.AddLabel("Ported by and copied from vOID github.com/voidbuddy");
+            Menu.AddSeparator();
+            Menu.AddLabel("Edited by Darakath");
+            Menu.AddLabel("https://www.elobuddy.net/user/1112-darakath/");
 
+            cMenu = Menu.AddSubMenu("Configuration", "config");
+            cMenu.AddGroupLabel("Config");
+            cMenu.Add("mouseRange", new Slider("Range from Mouse to GameObject", 400, 100, 1000));
 
-
-            Config = menu.AddSubMenu("Config", "config");
-            Config.AddGroupLabel("Config");
-            Config.Add("range", new Slider("Range from mouse to obj", 400, 100, 1000));
-
-
+            cMenu.AddLabel("Text Color");
+            cMenu.Add("colorR", new Slider("R", 255, 0 , 255));
+            cMenu.Add("colorG", new Slider("G", 255, 0, 255));
+            cMenu.Add("colorB", new Slider("B", 255, 0, 255));
 
             Game.OnTick += Game_OnTick;
             Drawing.OnDraw += OnDraw;
-
         }
 
         private static void Game_OnTick(EventArgs args)
         {
-            if (Environment.TickCount - _lastUpdateTick > 150)
+            if (Environment.TickCount - lastTick > 150)
             {
-                _lstGameObjects = ObjectManager.Get<GameObject>().ToList();
-                _lstObjCloseToMouse =
-                    _lstGameObjects.Where(o => o.Position.Distance(Game.CursorPos) < Config["range"].Cast<Slider>().CurrentValue && !(o is Obj_Turret) && o.Name != "missile" && !(o is Obj_LampBulb) && !(o is Obj_SpellMissile) && !(o is GrassObject) && !(o is DrawFX) && !(o is LevelPropSpawnerPoint) && !(o is Obj_GeneralParticleEmitter) && !o.Name.Contains("MoveTo")).ToList();
-                _lastUpdateTick = Environment.TickCount;
+                gameObjects = ObjectManager.Get<GameObject>().ToList();
+                mouseObjects =
+                    gameObjects.Where(
+                        o =>
+                        o.Position.Distance(Game.CursorPos) < cMenu["mouseRange"].Cast<Slider>().CurrentValue
+                        && !(o is Obj_Turret) && o.Name != "missile" && !(o is Obj_LampBulb) && !(o is Obj_SpellMissile)
+                        && !(o is GrassObject) && !(o is DrawFX) && !(o is LevelPropSpawnerPoint)
+                        && !(o is Obj_GeneralParticleEmitter) && !o.Name.Contains("MoveTo")).ToList();
+                lastTick = Environment.TickCount;
             }
         }
 
         private static void OnDraw(EventArgs args)
         {
-            foreach (var obj in _lstObjCloseToMouse)
+            foreach (var obj in mouseObjects)
             {
-                if (!obj.IsValid) return;
-                var X = Drawing.WorldToScreen(obj.Position).X;
-                var Y = Drawing.WorldToScreen(obj.Position).Y;
-                Drawing.DrawText(X, Y + 10, System.Drawing.Color.Red, obj.Type.ToString());
-                Drawing.DrawText(X, Y + 20, System.Drawing.Color.Red, "NetworkID: " + obj.NetworkId);
-                Drawing.DrawText(X, Y + 30, System.Drawing.Color.Red, obj.Position.ToString());
+                if (!obj.IsValid)
+                {
+                    return;
+                }
+                var x = Drawing.WorldToScreen(obj.Position).X;
+                var y = Drawing.WorldToScreen(obj.Position).Y;
+                Drawing.DrawText(x, y + 10, GetMenuColor(), obj.Type.ToString());
+                Drawing.DrawText(x, y + 20, GetMenuColor(), "NetworkID: " + obj.NetworkId);
+                Drawing.DrawText(x, y + 30, GetMenuColor(), obj.Position.ToString());
                 if (obj is Obj_AI_Base)
                 {
                     var aiobj = obj as Obj_AI_Base;
-                    Drawing.DrawText(X, Y + 40, System.Drawing.Color.Red, "Health: " + aiobj.Health + "/" + aiobj.MaxHealth + "(" + aiobj.HealthPercent + "%)");
+                    Drawing.DrawText(
+                        x,
+                        y + 40,
+                        GetMenuColor(),
+                        "Health: " + aiobj.Health + "/" + aiobj.MaxHealth + "(" + aiobj.HealthPercent + "%)");
                 }
                 if (obj is AIHeroClient)
                 {
                     var hero = obj as AIHeroClient;
-                    Drawing.DrawText(X, Y + 50, System.Drawing.Color.Red, "Spells:");
-                    Drawing.DrawText(X, Y + 60, System.Drawing.Color.Red, "(Q): " + hero.Spellbook.Spells[0].Name);
-                    Drawing.DrawText(X, Y + 70, System.Drawing.Color.Red, "(W): " + hero.Spellbook.Spells[1].Name);
-                    Drawing.DrawText(X, Y + 80, System.Drawing.Color.Red, "(E): " + hero.Spellbook.Spells[2].Name);
-                    Drawing.DrawText(X, Y + 90, System.Drawing.Color.Red, "(R): " + hero.Spellbook.Spells[3].Name);
-                    Drawing.DrawText(X, Y + 100, System.Drawing.Color.Red, "(D): " + hero.Spellbook.Spells[4].Name);
-                    Drawing.DrawText(X, Y + 110, System.Drawing.Color.Red, "(F): " + hero.Spellbook.Spells[5].Name);
+                    Drawing.DrawText(x, y + 50, GetMenuColor(), "Spells:");
+                    Drawing.DrawText(x, y + 60, GetMenuColor(), "(Q): " + hero.Spellbook.Spells[0].Name);
+                    Drawing.DrawText(x, y + 70, GetMenuColor(), "(W): " + hero.Spellbook.Spells[1].Name);
+                    Drawing.DrawText(x, y + 80, GetMenuColor(), "(E): " + hero.Spellbook.Spells[2].Name);
+                    Drawing.DrawText(x, y + 90, GetMenuColor(), "(R): " + hero.Spellbook.Spells[3].Name);
+                    Drawing.DrawText(x, y + 100, GetMenuColor(), "(D): " + hero.Spellbook.Spells[4].Name);
+                    Drawing.DrawText(x, y + 110, GetMenuColor(), "(F): " + hero.Spellbook.Spells[5].Name);
                     var buffs = hero.Buffs;
                     if (buffs.Any())
                     {
-                        Drawing.DrawText(X, Y + 120, System.Drawing.Color.Red, "Buffs:");
+                        Drawing.DrawText(x, y + 120, GetMenuColor(), "Buffs:");
                     }
                     for (var i = 0; i < buffs.Count() * 10; i += 10)
                     {
-                        Drawing.DrawText(X, (Y + 130 + i), System.Drawing.Color.Red, buffs[i / 10].Count + "x " + buffs[i / 10].Name);
+                        Drawing.DrawText(x, (y + 130 + i), GetMenuColor(), buffs[i / 10].Count + "x " + buffs[i / 10].Name);
                     }
-
                 }
                 if (obj is Obj_SpellMissile)
                 {
                     var missile = obj as Obj_SpellMissile;
-                    Drawing.DrawText(X, Y + 40, System.Drawing.Color.Red, "Missile Speed: " + missile.SData.MissileSpeed);
-                    Drawing.DrawText(X, Y + 50, System.Drawing.Color.Red, "Cast Range: " + missile.SData.CastRange);
+                    Drawing.DrawText(x, y + 40, GetMenuColor(), "Missile Speed: " + missile.SData.MissileSpeed);
+                    Drawing.DrawText(x, y + 50, GetMenuColor(), "Cast Range: " + missile.SData.CastRange);
                 }
 
                 if (obj is MissileClient && obj.Name != "missile")
                 {
                     var missile = obj as MissileClient;
-                    Drawing.DrawText(X, Y + 40, System.Drawing.Color.Red, "Missile Speed: " + missile.SData.MissileSpeed);
-                    Drawing.DrawText(X, Y + 50, System.Drawing.Color.Red, "Cast Range: " + missile.SData.CastRange);
+                    Drawing.DrawText(x, y + 40, GetMenuColor(), "Missile Speed: " + missile.SData.MissileSpeed);
+                    Drawing.DrawText(x, y + 50, GetMenuColor(), "Cast Range: " + missile.SData.CastRange);
                 }
             }
         }
     }
 }
-
--- DeveloperBuddy/Properties/AssemblyInfo.cs
+++ DeveloperBuddy/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AddonTemplate")]
+[assembly: AssemblyTitle("DevBuddy2.0")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AddonTemplate")]
+[assembly: AssemblyProduct("DevBuddy2.0")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3203502a-4253-4edd-9331-74a49c616ed2")]
+[assembly: Guid("a381d8c5-f439-4f0f-a61b-c62bc4c57786")]
 
 // Version information for an assembly consists of the following four values:
 //
